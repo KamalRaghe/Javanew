e307177e4bf88d8cc35e6c0fe0160a539beb92eb
Kamal <kamal.raghe33@gmail.com>
2023-02-14 14:36:40 -0500

    Java

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+for ( let i=1; i<=20;i++){
+    console.log(i);
+    if (i %3 === 000 && i%5===0) {
+        console.log(`${i} --> Frontend simplified`);
+    }
+    else if (i%5===0){
+        console.log(`${i} --> Simplified`);
+    }
+    else if(i%3===0 )
+        console.log(`${i} --> Frontend`);
+    }
